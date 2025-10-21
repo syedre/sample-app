7752f4bc1e2781572682bf353acb36665859a024
syedre <syed.rehan123@gmail.com>
2025-10-21 18:18:52 +0530

    added user profile

--- app/components/userProfile.jsx
+++ app/components/userProfile.jsx
@@ -1,9 +1,27 @@
 "use client";
 
 import { LogOut, Panda } from "lucide-react";
-import { Button } from "@/components/ui/button";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState } from "react";
 
 export default function UserMenu() {
+  const [user] = useState({
+    name: "Rehan Ahmed",
+    email: "rehan@example.com",
+    avatar:
+      "https://mumbai-test-express.s3.ap-south-1.amazonaws.com/uploads/0b06d363-05fd-4248-99bb-33bd2e2cbfc7.png", // or a placeholder image
+  });
+
   const handleLogout = () => {
     localStorage.removeItem("token");
     window.location.href = "/"; // redirect to login
@@ -12,15 +30,32 @@ export default function UserMenu() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-white border-b ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between">
-        <Panda className="h-8 w-8 text-black" />
-        <Button
-          variant="outline"
-          onClick={handleLogout}
-          className="flex items-center gap-2"
-        >
-          <LogOut className="w-4 h-4" />
-          Logout
-        </Button>
+        {/* <Panda className="h-8 w-8 text-black" /> */}
+        <div></div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div className="flex items-center gap-2 cursor-pointer">
+              <Avatar>
+                <AvatarImage src={user.avatar} alt={user.name} />
+                <AvatarFallback>{user.name.charAt(0)}</AvatarFallback>
+              </Avatar>
+            </div>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent className="w-56 mt-2">
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            {/* <DropdownMenuSeparator /> */}
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <span className="font-medium">{user.name}</span>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={handleLogout} className="text-red-500">
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </header>
   );
