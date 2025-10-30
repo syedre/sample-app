141d4a1bdd576b783ad61f8d88f836f96efe42ee
syedre <syed.rehan123@gmail.com>
2025-10-30 08:35:55 +0530

    added list todos api

--- app/apis/todos.js
+++ app/apis/todos.js
@@ -31,3 +31,19 @@ export async function updateTodo(id, name, description) {
   );
   return response;
 }
+
+export async function listTodos() {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    window.location.href = "/"; // redirect to login if not logged in
+    return;
+  }
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/listtodos`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = await response.json();
+  return data;
+}
--- app/todos/page.jsx
+++ app/todos/page.jsx
@@ -1,8 +1,9 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 
 import TodoList from "../components/todolist";
 import UserMenu from "../components/userProfile";
+import { listTodos } from "../apis/todos";
 
 export default function TodosPage() {
   const [todos, setTodos] = useState([]);
@@ -10,24 +11,17 @@ export default function TodosPage() {
   const [userData, setUserData] = useState(null);
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      window.location.href = "/"; // redirect to login if not logged in
-      return;
-    }
-
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/listtodos`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
+    async function fetchData() {
+      try {
+        const data = await listTodos();
         setTodos(data?.todos);
         setFilteredTodos(data?.todos);
         setUserData(data?.user);
-      })
-      .catch((err) => console.error("Error fetching:", err));
+      } catch (err) {
+        console.error("Error fetching:", err);
+      }
+    }
+    fetchData();
   }, []);
 
   return (
