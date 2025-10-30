171b733774015d41e29efec59511bb6599705510
syedre <syed.rehan123@gmail.com>
2025-10-30 17:56:42 +0530

    added constants

--- app/constants/forms.js
+++ app/constants/forms.js
@@ -1,10 +1,15 @@
+export const F_EMAIL = "email";
+export const F_PASSWORD = "password";
+export const F_NAME = "name";
+export const F_TEXT = "text";
+
 export const Login_Form = [
-  { type: "text", label: "email" },
-  { type: "password", label: "password" },
+  { type: F_TEXT, label: F_EMAIL },
+  { type: F_PASSWORD, label: F_PASSWORD },
 ];
 
 export const Sign_Up_Form = [
-  { type: "text", label: "name" },
-  { type: "text", label: "email" },
-  { type: "password", label: "password" },
+  { type: F_TEXT, label: F_NAME },
+  { type: F_TEXT, label: F_EMAIL },
+  { type: F_PASSWORD, label: F_PASSWORD },
 ];
