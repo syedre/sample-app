ffef3da60df5e3a907092f99d43cf719dc321e84
syedre <syed.rehan123@gmail.com>
2025-11-05 18:20:32 +0530

    fixed dashboard prerender issue

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -3,16 +3,22 @@ import React, { Suspense } from "react";
 import SearchTable from "../(client-components)/search-table";
 
 async function DashboardPage() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/dashboard/api`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/dashboard`, {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    const text = await res.text();
+    throw new Error(`Fetch failed: ${res.status} ${text}`);
+  }
   const data = await res.json();
 
   return (
-    <>
+    <div>
       <h1 className="font-bold">Tables</h1>
       <Suspense fallback={<div>loading</div>}>
         <SearchTable table={data} />
       </Suspense>
-    </>
+    </div>
   );
 }
 export default DashboardPage;
